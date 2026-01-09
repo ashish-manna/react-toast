a8cf7bef9d67b4bd1970aeefc4c029686ec71f5e
ashish-manna <m.ashish1920@gmail.com>
2026-01-09 22:58:02 +0530

    code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,47 @@ import styles from "./App.module.css"
 function App() {
   const { NotificationComponent, triggerNotification } = useNotification("top-right");
   return (
-    <div className={styles.main}>
-      <div className={styles.section}>
-        <button className={styles.triggerButton}
-          onClick={() => {
-            triggerNotification({
-              type: "success",
-              message: "Success Text",
-              duration: 4000,
-            })
-          }}>Trigger Success</button>
-        <button className={styles.triggerButton}
-          onClick={() => {
-            triggerNotification({
-              type: "error",
-              message: "Error Text",
-              duration: 4000,
-            })
-          }}>Trigger Error</button>
+    <div className={styles.app}>
+      <h1>React toastify is here...</h1>
+      <div className={styles.main}>
+        <div className={styles.section}>
+          <button className={styles.triggerButton}
+            onClick={() => {
+              triggerNotification({
+                type: "success",
+                message: "Success Text",
+                duration: 4000,
+              })
+            }}>Trigger Success</button>
+          <button className={styles.triggerButton}
+            onClick={() => {
+              triggerNotification({
+                type: "error",
+                message: "Error Text",
+                duration: 4000,
+              })
+            }}>Trigger Error</button>
+        </div>
+        <div className={styles.section}>
+          <button className={styles.triggerButton}
+            onClick={() => {
+              triggerNotification({
+                type: "info",
+                message: "Info Text",
+                duration: 4000,
+              })
+            }}>Trigger Info</button>
+          <button className={styles.triggerButton}
+            onClick={() => {
+              triggerNotification({
+                type: "warning",
+                message: "Warning Text",
+                duration: 4000,
+              })
+            }}>Trigger Warning</button>
+        </div>
+        {NotificationComponent}
       </div>
-      <div className={styles.section}>
-        <button className={styles.triggerButton}
-          onClick={() => {
-            triggerNotification({
-              type: "info",
-              message: "Info Text",
-              duration: 4000,
-            })
-          }}>Trigger Info</button>
-        <button className={styles.triggerButton}
-          onClick={() => {
-            triggerNotification({
-              type: "warning",
-              message: "Warning Text",
-              duration: 4000,
-            })
-          }}>Trigger Warning</button>
-      </div>
-      {NotificationComponent}
     </div>
   )
 }
